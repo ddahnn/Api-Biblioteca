96fadb5a80977f5d5dee46b94f4d08fe0ac2c67a
dahn <ddahnn@gmail.com>
2025-07-08 08:47:56 -0300

    atualização cors

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from config.database import Base, engine
 from routes import autor_routes, livro_routes, cliente_routes, emprestimo_routes
 
 
+from fastapi import FastAPI # type: ignore
+from fastapi.middleware.cors import CORSMiddleware # type: ignore
+from config.database import Base, engine
+from routes import autor_routes, livro_routes, cliente_routes, emprestimo_routes
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
@@ -11,6 +16,16 @@ app = FastAPI(
     description="API para gerenciamento de biblioteca (autores, livros, clientes, empréstimos)."
 )
 
+# Configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Rotas
 app.include_router(cliente_routes.router)
 app.include_router(autor_routes.router)
 app.include_router(livro_routes.router)
